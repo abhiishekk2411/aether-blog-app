9e9eae234464567aedc8000baabfbb8588867370
Abhishek <choudharyabhishek242004@gmail.com>
2026-07-22 16:57:38 +0530

    added blogcard ,newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/blog' element={<Blog />} />
+      <Route path='/blog/:_id' element={<Blog />} />
     </Routes>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../components/NavBar.jsx'
 import Header from '../components/Header.jsx'
 import BlogList from '../components/BlogList.jsx'
+import Newsletter from '../components/Newsletter.jsx'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <NavBar/>
       <Header/>
       <BlogList/>
+      <Newsletter/>
     </>
   )
 }
