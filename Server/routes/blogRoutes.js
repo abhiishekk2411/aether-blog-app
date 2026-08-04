1ec9312c7a9020136e7c59eca5bb3a1f370379a5
Abhishek <choudharyabhishek242004@gmail.com>
2026-08-04 11:46:21 +0530

    added multer middleware

---
+++ Server/routes/blogRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { addBlog } from '../controllers/blogController';
+import upload from '../middlewares/multer';
+
+const blogRouter=express.Router();
+
+blogRouter.post("/add",upload.single('image'),addBlog);
