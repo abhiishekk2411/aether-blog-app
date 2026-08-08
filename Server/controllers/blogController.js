6c71fa107d5e24051b909084a5bb16e4d1b38da1
Abhishek <choudharyabhishek242004@gmail.com>
2026-08-08 15:03:52 +0530

    set up blogControllers and blogRoutes

--- Server/controllers/blogController.js
+++ Server/controllers/blogController.js
@@ -2,43 +2,95 @@ import fs from 'fs';
 import imageKit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js';
 
+export const addBlog = async (req, res) => {
+    try {
+        const { title, subtitle, description, category, isPublished } =
+            JSON.parse(req.body.blog);
 
-export const addBlog=async(req,res)=>{
-    try{
-        const {title,subtitle,description,category,isPublished}=JSON.parse(req.body.blog);
-        const imageFile=req.file;
+        const imageFile = req.file;
 
-        if(!title || !description || !category || !imageFile || !isPublished){
-            return res.status(400).json({message:"All fields are required"});
+        if (!title || !description || !category || !imageFile || isPublished === undefined) {
+            return res.status(400).json({ message: "All fields are required" });
         }
 
-        const fileBuffer=fs.readFileSync(imageFile.path);
-        //upload image to imagekit
-        const response=await imageKit.upload({
-            file:fileBuffer,
-            fileName:imageFile.originalname,
-            folder:"/blogs"
-        })
+        const response = await imageKit.files.upload({
+            file: fs.createReadStream(imageFile.path),
+            fileName: imageFile.originalname,
+            folder: "/blogs"
+        });
 
-        //optimiaze image url
-        const optimizedImageUrl=imageKit.url({
-            path:response.filePath,
-            transformation:[
-                {quality:"auto"},                  //auto compression
-                {format:"webp"},                   //convert to webp format
-                {width:1280}                     //width resizing
-            ]
-        })
-        
-        const imageUrl=optimizedImageUrl;
+        if (imageFile.path) {
+            fs.unlinkSync(imageFile.path);
+        }
 
-        //saving data to mongodb database
-        await Blog.create({title,subtitle,description,category,imageUrl,isPublished});
-        
-        res.json({success:true,message: "blog uploaded successfuly"});
+        await Blog.create({
+            title,
+            subtitle,
+            description,
+            category,
+            image: response.url,
+            isPublished
+        });
+
+        res.json({
+            success: true,
+            message: "blog uploaded successfully"
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
-    catch(error){
-         res.json({success:false,message: error.message});
+};
 
-     }
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find({ isPublished: true });
+        res.json({ success: true, blogs })
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export const getBlogsbyId = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId);
+
+        if (!blog) {
+            return res.json({ success: false, message: "Blog not found" });
+        }
+        res.json({ success: true, blog });
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+
+    }
+}
+
+export const deleteBlogsbyId = async (req, res) => {
+    try {
+        const { id } = req.body;
+        await Blog.findByIdAndDelete(id);
+        res.json({ success: true, message: "Blog Deleted SuccessFully" });
+
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+
+    }
+}
+
+export const togglePublish = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished;
+        await blog.save();
+        res.json({ success: true, message: "Blog Status Updated" });
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+
+    }
 }
--- Server/routes/blogRoutes.js
+++ Server/routes/blogRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { addBlog } from '../controllers/blogController.js';
+import { addBlog, deleteBlogsbyId, getAllBlogs, getBlogsbyId, togglePublish } from '../controllers/blogController.js';
 import upload from '../middlewares/multer.js';
 import auth from '../middlewares/auth.js';
 
 const blogRouter=express.Router();
 
 blogRouter.post("/add",upload.single('image'),auth,addBlog);
+blogRouter.get("/all",getAllBlogs);
+blogRouter.get('/:blogId',getBlogsbyId);
+blogRouter.post('/delete',auth,deleteBlogsbyId);
+blogRouter.post('/toggle-Publish',auth,togglePublish);
 
 export default blogRouter;
